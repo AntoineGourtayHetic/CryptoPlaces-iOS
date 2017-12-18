17510b831a5b5357a1969a829b523707b1fa4f69
AntoineGourtayHetic <antoine.gourtay@hetic.net>
2017-12-18 09:43:33 +0100

    :tada: initial commit

---
+++ mob-e16-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mob-e16-ios
+//
+//  Created by Antoine Gourtay on 18/12/2017.
+//  Copyright © 2017 Antoine Gourtay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
